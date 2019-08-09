4b1616fe2e7211ea7aa17cd66eb9a4e93e7a9f89
irac <ding.dongsheng@fengniaowu.com>
2019-08-09 18:30:13 +0800

    Add souce file Prallel

---
+++ Multithreading/PLinq.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PLinq类
+{
+    public class Test
+    {
+        protected readonly ITestOutputHelper Output;
+        object lockObject = new object();
+        public void WriteLine(string message)
+        {
+            Trace.WriteLine(message);
+            Output.WriteLine(message);
+        }
+        public Test(ITestOutputHelper tempOutput)
+        {
+            Output = tempOutput;
+        }
+        public void PrintInfo(string typeName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(150));
+            WriteLine($"{typeName} type was printed on a thread id{Thread.CurrentThread.ManagedThreadId}");
+        }
+        public  string EmulateProcessing(string typeName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(150));
+            WriteLine($"{typeName} type was printed on a thread id{Thread.CurrentThread.ManagedThreadId}");
+            return typeName;
+        }
+        public IEnumerable<string> GetTypes()
+        {
+            return from assembly in
+                       AppDomain.CurrentDomain.GetAssemblies()
+                   from type in assembly.GetExportedTypes()
+                   where type.Name.StartsWith("Web")
+                   select type.Name;
+        }
+        public void MainTest()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var query = from t in GetTypes() select EmulateProcessing(t);
+            foreach(string typeName in query)
+            {
+                PrintInfo(typeName);
+            }
+            sw.Stop();
+            WriteLine(".........");
+            WriteLine("Sequential LINQ query.");
+            WriteLine($"Time elapsed:{sw.Elapsed}");
+            WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+            Console.Clear();
+            sw.Reset();
+
+
+
+        }
+    }
+}
--- Multithreading/Parallel.cs
+++ Multithreading/Parallel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,6 +23,35 @@ namespace Parallel类
         public Test(ITestOutputHelper tempOutput)
         {
             Output = tempOutput;
+        }
+        public string EmulateProcessing(string taskName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(new Random(DateTime.Now.Millisecond).Next(250, 350)));
+            WriteLine($"{taskName} task was processed on a thread id {Thread.CurrentThread.ManagedThreadId}");
+            return taskName;
+        }
+        [Fact]
+        public void MainTest()
+        {
+            Parallel.Invoke(()=>EmulateProcessing("Task1"), () => EmulateProcessing("Task2"), () => EmulateProcessing("Task3"));
+            var cts = new CancellationTokenSource();
+            var result = Parallel.ForEach(Enumerable.Range(1, 30), new ParallelOptions
+            {
+                CancellationToken = cts.Token,
+                MaxDegreeOfParallelism = Environment.ProcessorCount,
+                TaskScheduler = TaskScheduler.Default
+            }, (i, state) =>
+             {
+                 WriteLine(i.ToString());
+                 if (i == 20)
+                 {
+                     state.Break();
+                     WriteLine($"Loop is stopped:{state.IsStopped}");
+                 }
+             });
+            WriteLine("............");
+            WriteLine($"IsCompleted：{result.IsCompleted} ");
+            WriteLine($"Lowest break iteration:{result.LowestBreakIteration}");
         }
     }
 }
